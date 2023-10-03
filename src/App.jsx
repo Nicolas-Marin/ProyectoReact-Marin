c7b5f67fc6b2797bac8d3ab9b46520215139d41d
Nicolas-Marin <marinnicolas856@gmail.com>
2023-10-02 23:35:42 -0300

    Segunda Pre-Entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import NavBar from './components/NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <ItemListContainer greeting={'Bienvenidos a la tienda!'} color={'oscuro'}/>
+      <ItemCount initial={1} stock={15} onAdd={(quantity)=> console.log('Cantidad Agregada', quantity)}/>
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react"
+import { getProducts } from "../../asyncMook"
+import ItemList from "../ItemList/ItemList"
+
 const ItemListContainer = ({ greeting, color }) =>{
+    const [products, setProducts] = useState([])
+
+    useEffect(()=>{
+        getProducts()
+        .then(Response => {
+            setProducts(Response)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }, [])
+
     return(
         <div className="centrado">
             <h1 className={color} >{greeting}</h1>
+            <ItemList products={products}/>
         </div>
 
     )
